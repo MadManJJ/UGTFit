2adbce77becda94c7589bad0ecf9cbe8b5097e34
Pannawich Thamart <MadManJJ4949@gmail.com>
2025-01-21 13:54:20 +0700

    fix cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,7 +63,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       path: "/",
     });
     return res
